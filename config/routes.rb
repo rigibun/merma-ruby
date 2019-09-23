df09a33ebf10080a78557dcde8a31d9c99b42c59
akihiro.anzai <mukojin.yoing@gmail.com>
2019-09-23 20:56:12 +0900

    Deviseで認証を仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
+  devise_for :users, path: '', controllers: {
+                       sessions: "users/sessions",
+                     }
+  root to: "static_pages#home"
+  get 'sign_up' , to: 'devise/registrations#new'
 end
