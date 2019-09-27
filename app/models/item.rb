39229c56b69fec94f742a793a1de8b3d21570941
akihiro.anzai <mukojin.yoing@gmail.com>
2019-09-28 00:31:50 +0900

    Itemのnameのvalidateをpresenceとlengthのどちらかのみ出すように

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     enum condition: [:brand_new, :look_brand_new, :good, :fair, :poor]
-    validates :name, presence: true, length: { minimum: 1, maximum: 40 }
+    validates :name, presence: true
+    validates :name, length: { minimum: 1, maximum: 40 }, allow_blank: true
     validates :description, presence: true, length: { maximum: 1000 }
     validates :condition, presence: true
 
