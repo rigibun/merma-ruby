e8480b4540ff8322a8e24a66496ac52a4d55e25e
akihiro.anzai <mukojin.yoing@gmail.com>
2019-09-29 23:05:40 +0900

    ユーザにnameを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: {minimun: 4, maximum: 20}
 
   has_many :exhibits, class_name: "Item", foreign_key: "exhibitor_id"
 end
